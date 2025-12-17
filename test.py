e6227a78b4dd58a49bd8516073bbc5fd1fba99cd
Mrpoponep <hotu040304@gmail.com>
2025-12-17 21:32:59 +0700

    add

---
+++ test.py
@@ -0,0 +1,16 @@
+import torch
+from transformers import AutoModel, AutoTokenizer
+
+phobert = AutoModel.from_pretrained("vinai/phobert-base-v2")
+tokenizer = AutoTokenizer.from_pretrained("vinai/phobert-base-v2")
+
+# INPUT TEXT MUST BE ALREADY WORD-SEGMENTED!
+sentence = 'Chúng_tôi là những nghiên_cứu_viên .'  
+
+input_ids = torch.tensor([tokenizer.encode(sentence)])
+
+with torch.no_grad():
+    features = phobert(input_ids)  # Models outputs are now tuples
+
+print(features[0].shape)
+print(features[1].shape)
